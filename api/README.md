340b37a9fee76a6ee26658401e41b88900d6c739
srobertson <srobertson@acromediainc.com>
2016-02-27 10:30:58 -0800

    Initial commit of API files from generator.

---
+++ api/README.md
@@ -0,0 +1,19 @@
+# api
+
+Lengthenr API
+
+[![Dependency Status](https://david-dm.org/s-robertson/api.svg)](https://david-dm.org/s-robertson/api)
+[![devDependency Status](https://david-dm.org/s-robertson/api/dev-status.svg?theme=shields.io)](https://david-dm.org/s-robertson/api#info=devDependencies)
+[![Build Status](https://travis-ci.org/s-robertson/api.svg?branch=master)](https://travis-ci.org/s-robertson/api)
+
+
+## Usage
+
+```bash
+$ echo "details coming soon"
+```
+
+
+## License
+
+MIT
---
+++ api/manifest.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const Confidence = require('confidence');
+const Config = require('./config');
+
+
+const criteria = {
+    env: process.env.NODE_ENV
+};
+
+
+const manifest = {
+    $meta: 'This file defines the plot device.',
+    server: {
+        debug: {
+            request: ['error']
+        },
+        connections: {
+            routes: {
+                security: true
+            }
+        }
+    },
+    connections: [{
+        port: Config.get('/port/api'),
+        labels: ['api']
+    }],
+    registrations: [
+        {
+            plugin: './server/api/index'
+        }
+    ]
+};
+
+
+const store = new Confidence.Store(manifest);
+
+
+exports.get = function (key) {
+
+    return store.get(key, criteria);
+};
+
+
+exports.meta = function (key) {
+
+    return store.meta(key, criteria);
+};
