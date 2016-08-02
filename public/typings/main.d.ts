940304833dedee86027633345ab6754a31c1220f
srobertson <srobertson@acromediainc.com>
2016-08-01 23:08:12 -0700

    Remove the old Angular app

--- public/typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
-/// <reference path="main/ambient/webpack/webpack.d.ts" />
-/// <reference path="main/ambient/zone.js/zone.js.d.ts" />
-/// <reference path="main/definitions/es6-promise/es6-promise.d.ts" />
